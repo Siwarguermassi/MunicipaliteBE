d21b14339ac0396032d2eff02d6589e6d538eaa3
Guermassi Siwar <guermassi7@gmail.com>
2020-06-18 15:35:18 +0200

    globalException

--- src/main/java/com/pfe/municipalite/dossier/controller/DossierController.java
+++ src/main/java/com/pfe/municipalite/dossier/controller/DossierController.java
@@ -4,10 +4,7 @@ import com.pfe.municipalite.dossier.entity.Dossier;
 import com.pfe.municipalite.dossier.service.DossierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("dossiers")
@@ -25,4 +22,11 @@ public class DossierController {
     public ResponseEntity<?> addDossier(@RequestBody Dossier dossier){
         return dossierService.ajouterDossier(dossier);
     }
+
+   @RequestMapping(value="/getById/{id}",method = RequestMethod.GET)
+    public  ResponseEntity<?> getDossierById(@PathVariable Long id){
+       return  dossierService.getDossierById(id);
+   }
+
+
 }
--- src/main/java/com/pfe/municipalite/dossier/service/DossierServiceImpl.java
+++ src/main/java/com/pfe/municipalite/dossier/service/DossierServiceImpl.java
@@ -2,6 +2,7 @@ package com.pfe.municipalite.dossier.service;
 
 import com.pfe.municipalite.dossier.entity.Dossier;
 import com.pfe.municipalite.dossier.repository.DossierRepository;
+import com.pfe.municipalite.globalException.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,9 @@ public class DossierServiceImpl implements DossierService{
     }
 
     @Override
-    public ResponseEntity<?> getDossierById(Long id) {
-       return null;
+    public ResponseEntity<?> getDossierById(Long id)
+    {
+        repository.findById(id).orElseThrow(()->new ProductNotFoundException("Dossier n'esxiste pas"));
+       return ResponseEntity.ok(repository.findById(id));
     }
 }
